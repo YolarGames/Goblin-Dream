9cac9c095c847cf596db57a3a7800c0eaa407a1b
Никита Иванин <nikitaivanin93@gmail.com>
2022-03-25 11:14:07 +0200

    Before Addressables installed

--- Assets/Scripts/Managers/LoadingManager.cs
+++ Assets/Scripts/Managers/LoadingManager.cs
@@ -1,48 +1,87 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-using System.Net;
+using System.Runtime.Serialization;
 using UnityEngine;
 using UnityEngine.Networking;
+using File = UnityEngine.Windows.File;
 
-public class LoadingManager : MonoBehaviour
+public class LoadingManager : MonoBehaviourSingleton<LoadingManager>
 {
-	[SerializeField] private string _companyName;
-	[SerializeField] private string _gameName;
-	[SerializeField] private string _fileType;
+	[SerializeField] private string _url;
+	[SerializeField] private string _name;
 	[SerializeField] private uint _version;
 	[SerializeField] private uint _crc;
-	[SerializeField] private BundlePath[] _bundles;
+	private const string FileType = ".unity3d";
 
+	private void Start()
+	{
+		StartCoroutine(FirstLaunchLoading());
+	}
 
 	private IEnumerator FirstLaunchLoading()
 	{
-		var bundle = _bundles[0];
-		var path = Path.Combine(
-			Application.persistentDataPath,
-			_companyName,
-			_gameName,
-			bundle.bundleName + _fileType);
-
-		#region Download bundle
-		using var webRequest = UnityWebRequestAssetBundle.GetAssetBundle(bundle.url, _version, _crc);
+		var path = Path.Combine(Application.persistentDataPath, _name + FileType);
+		Debug.Log("PATH: " + path);
+		
+		
+		using var webRequest = UnityWebRequestAssetBundle.GetAssetBundle(_url, _version, _crc);
 		yield return webRequest.SendWebRequest();
+		Debug.Log("Web request is done");
 
 		if (webRequest.result != UnityWebRequest.Result.Success)
 		{
 			Debug.Log(webRequest.error);
 			yield break;
 		}
-		#endregion
-
-		#region Save bundle
-		var handler = webRequest.downloadHandler;
-		File.WriteAllBytes(path, handler.data);
-		#endregion
 		
-		#region Load bundle from device
+		var assetBundle = DownloadHandlerAssetBundle.GetContent(webRequest);
+	}
 
-		#endregion
+	private void SerializeBundle(AssetBundle bundle, string path)
+	{
+		var binaryFormatter = GetBinaryFormatter();
+		var fileStream = new FileStream(path, FileMode.Create);
+		
+		binaryFormatter.Serialize(fileStream, bundle);
+		fileStream.Close();
+	}
 
+	private AssetBundle DeserializeBundle(string path)
+	{
+		if (!File.Exists(path))
+			return null;
+		
+		var binaryFormatter = GetBinaryFormatter();
+		var fileStream = new FileStream(path, FileMode.Open);
+
+		try
+		{
+			var bundle = binaryFormatter.Deserialize(fileStream) as AssetBundle;
+			fileStream.Close();
+			return bundle;
+		}
+		catch (Exception exception)
+		{
+			Debug.Log(exception.Message);
+		}
+
+		return null;
+	}
+
+	private BinaryFormatter GetBinaryFormatter()
+	{
+		var binaryFormatter = new BinaryFormatter();
+		// var surrogateSelector = new SurrogateSelector();
+		// var bundleSurrogate = new AssetBundleSurrogate();
+		
+		// surrogateSelector.AddSurrogate(
+		// 	type:typeof(AssetBundleSurrogate),
+		// 	context:new StreamingContext(StreamingContextStates.All),
+		// 	surrogate:bundleSurrogate);
+		
+		// binaryFormatter.SurrogateSelector = surrogateSelector;
+		return binaryFormatter;
 	}
 }
--- Assets/Scripts/ScriptableObjects/BundlePath.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "BundlePath", menuName = "BundlePath", order = 0)]
-public class BundlePath : ScriptableObject
-{
-	public string url;
-	public string bundleName;
-}
